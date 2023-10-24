69579bb5ac702888b2914d14e93408f0681e7693
kingdapster <sal.carrazco89@gmail.com>
2023-10-24 11:18:35 -0700

    sorry about the blank upload

---
+++ index.js
@@ -0,0 +1,30 @@
+//create html and js  index
+//create variables 
+//create equations for the secret code 10 - 40 -39
+// create alert for user to display warning and secret code
+// constants will be displayed as such (a1+a2) - (b1-b2) - (c1%c2)
+
+
+//  const a is number to equatoin 
+ const a1= 5
+
+ //cosnt a  is number of equation
+ const a2= 5
+
+ // cosnt b number of equation
+ const b1= 60
+
+ //const b number of equation
+ const b2= 20
+ // const c number of equation
+ const c1=79
+
+ // const c number of equation
+ const c2=40
+
+
+ confirm(`You have recieved this message because you have chosen ato open an importat vault.`)
+ alert(`Here is the seceret combination:${a1+a2}-${b1-b2}-${c1%c2}`)
+let result=prompt(`plaese enter combination`)
+console.log(result);
+
